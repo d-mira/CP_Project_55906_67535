b24198920b46086bee41bc03be9f8e01fbb3de01
Diogo Pinto <diogo.fp@sapo.pt>
2024-05-29 14:16:57 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "histogram_eq.h"
 #include <cstdlib>
+#include <chrono>
 
 int main(int argc, char **argv) {
 
@@ -15,9 +16,14 @@ int main(int argc, char **argv) {
 
     wbImage_t inputImage = wbImport(argv[1]);
     int n_iterations = static_cast<int>(std::strtol(argv[2], nullptr, 10));
+    auto start = std::chrono::high_resolution_clock::now();
     //wbImage_t outputImage = cp::iterative_histogram_equalization(inputImage, n_iterations);
     wbImage_t outputImage = cp::par::iterative_histogram_equalization_par(inputImage, n_iterations);
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = end - start;
     wbExport(argv[3], outputImage);
+
+    std::cout << "Execution time: " << elapsed.count();
 
     return 0;
 }
