7c13ea7b08f6d0909e3b3bf4b18ad67fe6807f6e
Diogo Pinto <diogo.fp@sapo.pt>
2024-05-29 12:53:11 +0100

    added all correct expected outputs, and created tests for parallelised version for each output
    - falta o output para sample_5184×3456.ppm com 1000 iteracoes.

--- test/histogram_eq_test.cpp
+++ test/histogram_eq_test.cpp
@@ -5,17 +5,56 @@
 using namespace cp;
 
 #define DATASET_FOLDER "../../dataset/"
-#define OUTPUT_DATASET_FOLDER "../../outputDataset"
+#define OUTPUT_DATASET_FOLDER "../../outputDataset/"
 
-TEST(HistogramEq, Input01_4) {
 
-    wbImage_t inputImage = wbImport(DATASET_FOLDER "input01.ppm");
-    wbImage_t outputImage = iterative_histogram_equalization(inputImage, 1);
-    // check if the output image is correct
+struct HistogramEqTestParams {
+    std::string inputFileName;
+    std::string outputFileName;
+    int iterations;
+};
 
-    wbImage_t baseOutput = wbImport(OUTPUT_DATASET_FOLDER "output01.ppm");
+class HistogramEqParTest : public ::testing::TestWithParam<HistogramEqTestParams> {};
+
+TEST_P(HistogramEqParTest, IterativeHistogramEqualizationPar) {
+    HistogramEqTestParams params = GetParam();
+
+    GTEST_LOG_(INFO) << "Testing parallelized version for " << params.inputFileName
+                     << " with " << params.iterations << " iterations";
+
+    wbImage_t inputImage = wbImport((DATASET_FOLDER + params.inputFileName).c_str());
+    wbImage_t outputImage = cp::par::iterative_histogram_equalization_par(inputImage, params.iterations);
+
+    std::string baseOutputPath = OUTPUT_DATASET_FOLDER + params.outputFileName;
+    wbImage_t baseOutput = wbImport(baseOutputPath.c_str());
 
     bool result = wbImage_sameQ(outputImage, baseOutput);
 
     EXPECT_TRUE(result);
+
+    std::cout << "Result: " << std::boolalpha << result << std::endl;
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    HistogramEqTests,
+    HistogramEqParTest,
+    ::testing::Values(
+        HistogramEqTestParams{"input01.ppm", "input01_1.ppm", 1},
+        HistogramEqTestParams{"input01.ppm", "input01_100.ppm", 100},
+        HistogramEqTestParams{"input01.ppm", "input01_500.ppm", 500},
+        HistogramEqTestParams{"input01.ppm", "input01_1000.ppm", 1000},
+        HistogramEqTestParams{"borabora_1.ppm", "borabora_1_1.ppm", 1},
+        HistogramEqTestParams{"borabora_1.ppm", "borabora_1_100.ppm", 100},
+        HistogramEqTestParams{"borabora_1.ppm", "borabora_1_500.ppm", 500},
+        HistogramEqTestParams{"borabora_1.ppm", "borabora_1_1000.ppm", 1000},
+        HistogramEqTestParams{"sample_5184×3456.ppm", "sample_5184×3456_1.ppm", 1},
+        HistogramEqTestParams{"sample_5184×3456.ppm", "sample_5184×3456_100.ppm", 100},
+        HistogramEqTestParams{"sample_5184×3456.ppm", "sample_5184×3456_500.ppm", 500},
+        HistogramEqTestParams{"sample_5184×3456.ppm", "sample_5184×3456_1000.ppm", 1000}
+    )
+);
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
